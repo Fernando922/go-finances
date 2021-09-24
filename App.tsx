3c0533ab2c998bed34d7e976f133e09c169126e9
Fernando de Paula <dipaula018@gmail.com>
2021-09-23 22:27:04 -0300

    feat: buttons

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import AppLoading from "expo-app-loading";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/global/styles/theme";
-import { Dashboard } from "./src/screens/Dashboard";
+import { Register } from "./src/screens/Register";
 
 import {
   useFonts,
@@ -24,7 +24,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   );
 }
