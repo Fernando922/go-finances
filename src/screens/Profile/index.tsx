41f038e48464756082e2e4ad4097a5d91f1c831c
Fernando de Paula <fernando.maciel@q2pay.com.br>
2022-01-18 21:19:04 -0300

    feat: init tests

---
+++ src/screens/Profile/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, TextInput, Button } from "react-native";
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>Perfil</Text>
+      <TextInput placeholder="Nome" autoCorrect={false} />
+      <TextInput placeholder="Sobrenome" autoCorrect={false} />
+
+      <Button title="Salvar" onPress={() => {}} />
+    </View>
+  );
+};
+
+export default Profile;
